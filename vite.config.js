40725848fe63a970f31f1e7fdf246f3bee1b1abb
Pranav77722 <162795931+Pranav77722@users.noreply.github.com>
2025-12-26 01:06:30 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/SIH_final_project_2025/' : '/',
+  }
 })
